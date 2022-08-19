13cabc92c78f08860479e3be1992e0bb30af141d
NikitaBez <bezmenov.nikita@gmail.com>
2022-08-19 22:40:32 +0900

    copy master branch

--- src/test/java/ru/netology/radioStationTest/RadioTest.java
+++ src/test/java/ru/netology/radioStationTest/RadioTest.java
@@ -1,56 +1,24 @@
-package ru.netology.radioStationTest;
+package ru.netology.radioStation;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import ru.netology.radioStation.Radio;
-public class RadioTest {
-   Radio radioStation = new Radio(10);
-    //Тесты на требование 1.1 пределы частот
-    @Test
+
+class RadioTest {
+    Radio radioStation = new Radio();
+
+    //тестируем требование 1.1
+    @Test //также подходит к требованию 1.4 (выставление номера радиостанции)
     public void shouldSetRadioStation() {
+        radioStation.setCurrentRadioStation(5);
 
-        radioStation.setCurrentRadioStation(8);
-
-        int expected = 8;
-        int actual = radioStation.getCurrentRadioStation();
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    public void shouldSetRadioStationMax() {
-
-        radioStation.setCurrentRadioStation(9);
-
-        int expected = 9;
+        int expected = 5;
         int actual = radioStation.getCurrentRadioStation();
 
         Assertions.assertEquals(expected, actual);
     }
-//проверка на изменение в ветке
+
     @Test
-    public void shouldSetRadioStationOutOfRange() {
-
-        radioStation.setCurrentRadioStation(10);
-
-        int expected = 0;
-        int actual = radioStation.getCurrentRadioStation();
-
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    public void shouldSetRadioStationMinLimitValues() {
-
-        radioStation.setCurrentRadioStation(-1);
-
-        int expected = 0;
-        int actual = radioStation.getCurrentRadioStation();
-
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    public void shouldSetRadioStationMin() {
-
+    public void shouldSetMinRadioStation() {
         radioStation.setCurrentRadioStation(0);
 
         int expected = 0;
@@ -58,9 +26,19 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
-    @Test
-    public void shouldSetRadioStationMinLimitValues2() {
 
+    @Test
+    public void shouldSetLimitValueMinRadioStation() {
+        radioStation.setCurrentRadioStation(-1);
+
+        int expected = 0;
+        int actual = radioStation.getCurrentRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetLimitValue2MinRadioStation() {
         radioStation.setCurrentRadioStation(1);
 
         int expected = 1;
@@ -69,91 +47,19 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
-
-
-    //требование 1.2 переключение next
     @Test
-    public void shouldSetNextRadioStation() {
-
-        radioStation.nextRadioStation(8);
+    public void shouldSetMaxRadioStation() {
+        radioStation.setCurrentRadioStation(9);
 
         int expected = 9;
-        int actual = radioStation.getCurrentRadioStation();
-
-        Assertions.assertEquals(expected,actual);
-    }
-
-    @Test
-    public void shouldSetNextRadioStationIfMax() {
-
-        radioStation.nextRadioStation(9);
-
-        int expected = 0;
-        int actual = radioStation.getCurrentRadioStation();
-
-        Assertions.assertEquals(expected,actual);
-    }
-    @Test
-    public void shouldSetNextRadioStationIfMaxLimitValue() {
-
-        radioStation.nextRadioStation(10);
-
-        int expected = 0;
-        int actual = radioStation.getCurrentRadioStation();
-
-        Assertions.assertEquals(expected,actual);
-    }
-
-    @Test
-    public void shouldSetNextRadioStationIfMinLimitValue() {
-
-        radioStation.nextRadioStation(-1);
-
-        int expected = 0;
-        int actual = radioStation.getCurrentRadioStation();
-
-        Assertions.assertEquals(expected,actual);
-    }
-
-    @Test
-    public void shouldSetNextRadioStationIfMin() {
-
-        radioStation.nextRadioStation(0);
-
-        int expected = 1;
-        int actual = radioStation.getCurrentRadioStation();
-
-        Assertions.assertEquals(expected,actual);
-    }
-    @Test
-    public void shouldSetNextRadioStationIfMinLimitValue2() {
-
-        radioStation.nextRadioStation(1);
-
-        int expected = 2;
-        int actual = radioStation.getCurrentRadioStation();
-
-        Assertions.assertEquals(expected,actual);
-    }
-
-
-
-    //требование 1.3 переключение prev
-    @Test
-    public void shouldSetPrevRadioStation(){
-
-        radioStation.prevRadioStation(8);
-
-        int expected = 7;
         int actual = radioStation.getCurrentRadioStation();
 
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldSetPrevRadioStationMax(){
-
-        radioStation.prevRadioStation(9);
+    public void shouldSetLimitValueMaxRadioStation() {
+        radioStation.setCurrentRadioStation(8);
 
         int expected = 8;
         int actual = radioStation.getCurrentRadioStation();
@@ -162,9 +68,20 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldSetPrevRadioStationOutOfRange(){
+    public void shouldSetLimitValue2MaxRadioStation() {
+        radioStation.setCurrentRadioStation(10);
 
-        radioStation.prevRadioStation(10);
+        int expected = 0; //ноль, так как проверяется условие установки произвольного канала,
+        int actual = radioStation.getCurrentRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //тестируем требование 1.2 NEXT
+    @Test
+    public void shouldSetNextRadioStation() {
+        radioStation.setCurrentRadioStation(8);
+        radioStation.nextRadioStation();
 
         int expected = 9;
         int actual = radioStation.getCurrentRadioStation();
@@ -173,30 +90,9 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldSetPrevRadioStationIfMin(){
-
-        radioStation.prevRadioStation(0);
-
-        int expected = 9;
-        int actual = radioStation.getCurrentRadioStation();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSetPrevRadioStationLimitValue(){
-
-        radioStation.prevRadioStation(-1);
-
-        int expected = 9;
-        int actual = radioStation.getCurrentRadioStation();
-
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    public void shouldSetPrevRadioStationLimitValue2(){
-
-        radioStation.prevRadioStation(1);
+    public void shouldSetNextMaxRadioStation() {
+        radioStation.setCurrentRadioStation(9);
+        radioStation.nextRadioStation();
 
         int expected = 0;
         int actual = radioStation.getCurrentRadioStation();
@@ -204,4 +100,182 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    //    @Test
+//    public void shouldSetLimitValueNextMaxRadioStation() {
+//        radioStation.setCurrentRadioStation(10);
+//        radioStation.nextRadioStation();
+//
+//        int expected = 1;
+//        int actual = radioStation.getCurrentRadioStation();
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void shouldSetLimitValueNextMinRadioStation() {
+//        radioStation.setCurrentRadioStation(-1);
+//        radioStation.nextRadioStation();
+//
+//        int expected = 1;
+//        int actual = radioStation.getCurrentRadioStation();
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+    @Test
+    public void shouldSetNextMinRadioStation() {
+        radioStation.setCurrentRadioStation(0);
+        radioStation.nextRadioStation();
+
+        int expected = 1;
+        int actual = radioStation.getCurrentRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //тестируем требование 1.3 PREV
+    @Test
+    public void shouldSetPrevRadioStation() {
+        radioStation.setCurrentRadioStation(8);
+        radioStation.prevRadioStation();
+
+        int expected = 7;
+        int actual = radioStation.getCurrentRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetPrevMaxRadioStation() {
+        radioStation.setCurrentRadioStation(9);
+        radioStation.prevRadioStation();
+
+        int expected = 8;
+        int actual = radioStation.getCurrentRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+//
+//    @Test
+//    public void shouldSetLimitValuePrevMaxRadioStation() {
+//        radioStation.setCurrentRadioStation(10);
+//        radioStation.prevRadioStation();
+//
+//        int expected = 9;
+//        int actual = radioStation.getCurrentRadioStation();
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void shouldSetLimitValueNPrevMinRadioStation() {
+//        radioStation.setCurrentRadioStation(-1);
+//        radioStation.prevRadioStation();
+//
+//        int expected = 9;
+//        int actual = radioStation.getCurrentRadioStation();
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+
+    @Test
+    public void shouldSetPrevMinRadioStation() {
+        radioStation.setCurrentRadioStation(0);
+        radioStation.prevRadioStation();
+
+        int expected = 9;
+        int actual = radioStation.getCurrentRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //тестируем требование 2.1 и 2.2
+    @Test
+    public void shouldIncreaseVolume() {
+        radioStation.setCurrentVolume(9);
+        radioStation.increaseVolume();
+
+        int expected = 10;
+        int actual = radioStation.getCurrentVolume();
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldIncreaseVolumeWhenAtMax() {
+        radioStation.setCurrentVolume(10);
+        radioStation.increaseVolume();
+
+        int expected = 10;
+        int actual = radioStation.getCurrentVolume();
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldIncreaseVolumeWhenAtMin() {
+        radioStation.setCurrentVolume(0);
+        radioStation.increaseVolume();
+
+        int expected = 1;
+        int actual = radioStation.getCurrentVolume();
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testingBoundaryValuesWhenAtMin() {
+        radioStation.setCurrentVolume(-1);
+        radioStation.increaseVolume();
+
+        int expected = 1;
+        int actual = radioStation.getCurrentVolume();
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testingBoundaryValuesWhenAtMax() {
+        radioStation.setCurrentVolume(11);
+        radioStation.increaseVolume();
+
+        int expected = 1;
+        int actual = radioStation.getCurrentVolume();
+
+        Assertions.assertEquals(expected,actual);
+    }
+    //тестируем требование 2.3
+    @Test
+    public void shouldReduceVolume() {
+        radioStation.setCurrentVolume(9);
+        radioStation.reduceVolume();
+
+        int expected = 8;
+        int actual = radioStation.getCurrentVolume();
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldReduceVolumeWhenAtMax() {
+        radioStation.setCurrentVolume(10);
+        radioStation.reduceVolume();
+
+        int expected = 9;
+        int actual = radioStation.getCurrentVolume();
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldReduceVolumeWhenAtMin() {
+        radioStation.setCurrentVolume(0);
+        radioStation.reduceVolume();
+
+        int expected = 0;
+        int actual = radioStation.getCurrentVolume();
+
+        Assertions.assertEquals(expected,actual);
+    }
+
 }
+
